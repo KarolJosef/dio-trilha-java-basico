c8eded528d922769aa7277f5339a9951fd42c322
KarolJosef <karoljozef123@hotmail.com>
2025-04-01 13:48:43 -0300

    Change repsitory name

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,16 @@
+public class ContaTerminal {
+   
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+      //TODO:Conhecer e importar a classe Sacnner
+
+      //Exibir as menagens para  nosso usuário
+
+      //Obter pela scanner os valores digitados no terminal
+
+      //Exibir a mensagem da conta criada
+    }
+}
